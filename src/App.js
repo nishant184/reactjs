1e3246e7fc257f57c3f139326967d24e4d9b936e
nishant184 <pardeshinishant57@gmail.com>
2020-02-21 22:52:26 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -3,11 +3,59 @@ import logo from './image.jpg';
 import './App.css';
 
 class App extends Component(){
+constructor(props){
+  super(props);
+  this.state={
+              newitem:"",
+              list:[]
+            }
+}
+additem(toadd){
+  if(toadd !==  '')
+  {
+    const newitem={
+                    id:Date.now(),
+                    value:this.additem,
+                    isDone:false
+                  }
+              const list=[...this.state.list];
+              list.push(newitem);
+              this.setstate(
+                {
+                  list,
+                  newitem:""
+                }
+              )  
+  }
+}
+deleteitem(id)
+{
+  const list=[...this.state.list];
+  const updatelist=list.filter(item=>item.id !=="");
+  this.setstate({list:updatelist});
+}
+updateinput(input)
+{
+  this.setstate({newitem:input});
+}
   render(){
     return(
       <div>     
          <h1>Hello</h1>
          <img src={logo} className="logo"/>
+         <div className="container">
+           add typess of disasters
+           <br/>
+           <input type="text" className="input-text" value={this.state.newitem} onChange={a=> this.updateinput(a.target.value)}/>
+           <button className="btn" onClick={() =>   this.additem(this.state.newitem)}>add disasters </button>
+           <div className="list">
+             <ul>
+               <li> <input type="Checkbox"  />avalanche</li>
+               <button className="btn">Delete</button>
+               
+            </ul>
+           </div>
+         </div>
      </div>
    
     );
